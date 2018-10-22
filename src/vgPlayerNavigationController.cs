89f5ec3a3facec9ab7626adb7492b7e56d603124
BARICHELLO <arturbarichello@hotmail.com>
2018-10-21 22:54:09 -0300

    Implement noclip speed
    Closes #1

--- src/vgPlayerNavigationController.cs
+++ src/vgPlayerNavigationController.cs
@@ -12,9 +12,21 @@ public class vgPlayerNavigationController : vgMonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Numlock)) {
             this.noclip = !this.noclip;
             if (this.noclip) {
-                this.gravity = -1f;
+                this.gravity = this.noclipGravity;
+                this.walkSpeed = this.noclipSpeed;
+                this.walkBackwardSpeed = this.noclipSpeed;
+                this.strafeSpeed= this.noclipSpeed;
+                this.jogSpeed = this.noclipSpeed;
+                this.jogBackwardSpeed = this.noclipSpeed;
+                this.jogStrafeSpeed = this.noclipSpeed;
             } else {
-                this.gravity = 9f;
+                this.gravity = this.defaultGravity;
+                this.walkSpeed = this.defaultWalkSpeed;
+                this.walkBackwardSpeed = this.defaultWalkBackwardSpeed;
+                this.strafeSpeed = this.defaultStrafeSpeed;
+                this.jogSpeed = this.defaultJogSpeed;
+                this.jogBackwardSpeed = this.defaultJogBackwardSpeed;
+                this.jogStrafeSpeed = this.defaultJogStrafeSpeed;
             }
         }
 
@@ -39,5 +51,16 @@ public class vgPlayerNavigationController : vgMonoBehaviour {
     }
 
     private bool noclip = false;
+
+    private float noclipGravity = -1f;
+    private float defaultGravity = 9f;
+
+    private float defaultWalkSpeed = 3f;
+    private float defaultWalkBackwardSpeed = 2f;
+    private float defaultStrafeSpeed = 3f;
+    private float defaultJogSpeed = 6f;
+    private float defaultJogBackwardSpeed = 4f;
+    private float defaultJogStrafeSpeed = 6f;
+
     private float noclipMovMultiplier = 3f;
 }
